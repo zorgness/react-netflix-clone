013a381250074103c32c2d9554b149c195faa4d7
Thomas Fouquert <tfouquert@gmail.com>
2022-12-09 17:46:52 +0100

    update on css

--- src/components/NetflixApp.js
+++ src/components/NetflixApp.js
@@ -2,7 +2,9 @@ import React from 'react'
 import netflixLogo from '../images/netflix-logo.png'
 import netflixAvatar from '../images/netflix-avatar.png'
 import netflixSample from '../images/sample.jpg'
+import netflixSample2 from '../images/sample1.jpg'
 import netflixSamplePoster from '../images/sample-poster.jpg'
+import netflixSamplePoster2 from '../images/sample-poster1.jpg'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
@@ -54,15 +56,15 @@ const NetflixApp = () => {
         </Toolbar>
       </AppBar>
 
-      <header>
-        <div>
-          <h1>La casa del papel</h1>
-          <div>
-            <button>Lecture</button>
+      <header className="banner">
+        <div className="banner__contents">
+          <h1 className="banner__title">La casa del papel</h1>
+          <div className="banner__buttons">
+            <button className="banner__button">Lecture</button>
 
-            <button>Ajouter à ma liste</button>
+            <button className="banner__button">Ajouter à ma liste</button>
           </div>
-          <h1>
+          <h1 className="synopsis">
             Le Professeur recrute une jeune braqueuse et sept autres criminels
             en vue d'un cambriolage grandiose ciblant la Maison royale de la
             Monnaie d'Espagne.
@@ -70,21 +72,39 @@ const NetflixApp = () => {
         </div>
       </header>
 
-      <div>
+      <div className="row">
         <h2>Films Netflix</h2>
-        <div>
-          <img src={netflixSample} alt="" height={250} />
+        <div className="row__posters">
+          <img
+            src={netflixSample}
+            alt=""
+            className="row__poster row__posterLarge"
+          />
+          <img
+            src={netflixSample2}
+            alt=""
+            className="row__poster row__posterLarge"
+          />
         </div>
       </div>
 
       <div>
         <h2>Série NetFlix</h2>
-        <div>
-          <img src={netflixSamplePoster} alt="" height={300} />
+        <div className="row__posters">
+          <img
+            src={netflixSamplePoster}
+            alt=""
+            className="row__poster row__posterLarge"
+          />
+          <img
+            src={netflixSamplePoster2}
+            alt=""
+            className="row__poster row__posterLarge"
+          />
         </div>
       </div>
 
-      <footer>2021 - Netflix Clone</footer>
+      <footer>2022 - Netflix Clone</footer>
     </div>
   )
 }
