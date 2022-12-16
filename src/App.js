dc1c774044231ada9e2526e5bcb114af334a7f23
Thomas Fouquert <tfouquert@gmail.com>
2022-12-16 18:09:44 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css'
+import {ErrorBoundary} from 'react-error-boundary'
+import {NetflixAppBar} from 'components/NetflixAppBar'
 import {NetflixApp} from 'components/NetflixApp'
 
 function App() {
