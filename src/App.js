99131a90bbe866e5daf61641d1f974d870381850
Thomas Fouquert <tfouquert@gmail.com>
2022-12-23 20:35:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import ErrorFallback from 'ErrorFallback'
 import {ThemeProvider} from '@mui/styles'
 import {createTheme} from '@mui/material/styles'
 import {NetflixApp} from 'components/NetflixApp'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {NetflixMovies} from 'components/NetflixMovies'
+import {NetflixSeries} from 'components/NetflixSeries'
+import {NetflixNews} from 'components/NetflixNews'
 
 const theme = createTheme({
   palette: {
@@ -21,6 +25,14 @@ function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<NetflixApp />} />
+            <Route exact path="/movies" element={<NetflixMovies />} />
+            <Route exact path="/series" element={<NetflixSeries />} />
+            <Route exact path="/news" element={<NetflixNews />} />
+          </Routes>
+        </Router>
         <NetflixApp />
       </ThemeProvider>
     </ErrorBoundary>
---
+++ src/components/NetflixMovies.js
@@ -0,0 +1,104 @@
+import React, {useState, useEffect} from 'react'
+import {getRandomType, getRandomId} from 'utils/helper'
+import NetflixAppBar from './NetflixAppBar'
+import NetflixHeader from './NetflixHeader'
+import NetflixFooter from './NetflixFooter'
+import NetflixRow from './NetflixRow'
+import {clientApi} from 'utils/clientApi'
+import {Alert, AlertTitle} from '@mui/material'
+import CircularProgress from '@mui/material/CircularProgress'
+import {makeStyles} from '@mui/styles'
+import {useFetchData} from 'utils/hooks'
+import {TYPE_MOVIE, TYPE_TV} from 'config'
+
+const useStyles = makeStyles({
+  alert: {
+    width: '50%',
+    margin: 'auto',
+    marginBotton: '50px',
+  },
+  progress: {
+    marginLeft: '30px',
+  },
+})
+
+const NetflixMovies = () => {
+  const [type] = useState(getRandomType())
+
+  const defaultMovieId = getRandomId(type)
+
+  const {data: headerMovie, status, error, execute} = useFetchData()
+
+  useEffect(() => {
+    execute(clientApi(`${type}/${defaultMovieId}`))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (status === 'error') {
+    throw new Error(error.message)
+  }
+
+  return (
+    <>
+      <NetflixAppBar />
+      <NetflixHeader movie={headerMovie} type={type} />
+
+      <NetflixRow
+        wideImage={true}
+        watermark={true}
+        type={TYPE_MOVIE}
+        filter={'trending'}
+        title="Films Netflix"
+      />
+
+      <NetflixRow
+        wideImage={false}
+        watermark={true}
+        type={TYPE_TV}
+        filter={'trending'}
+        title="Série Netflix"
+      />
+
+      <NetflixRow
+        wideImage={true}
+        watermark={true}
+        type={TYPE_MOVIE}
+        filter={'toprated'}
+        title="Les mieux notés"
+      />
+
+      <NetflixRow
+        wideImage={true}
+        watermark={true}
+        type={TYPE_TV}
+        param={10759}
+        filter="genre"
+        title="Action & aventure"
+      />
+
+      <NetflixRow
+        wideImage={true}
+        watermark={true}
+        type={TYPE_MOVIE}
+        param={53}
+        filter="genre"
+        title="Les meilleurs Thrillers"
+      />
+      {status === 'error' ? (
+        <div className="alert">
+          <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            This is an error alert — <strong>check it out!</strong>
+          </Alert>
+        </div>
+      ) : null}
+      {status === 'fetching' ? (
+        <div className="progress">
+          <CircularProgress />
+        </div>
+      ) : null}
+      <NetflixFooter />
+    </>
+  )
+}
+export {NetflixMovies}
