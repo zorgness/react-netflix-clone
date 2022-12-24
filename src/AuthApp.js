4c4fb4f17c879bd3ab815e41d39f0a9cdded629c
Thomas Fouquert <tfouquert@gmail.com>
2022-12-24 10:51:15 +0100

    update

---
+++ src/AuthApp.js
@@ -0,0 +1,32 @@
+import {NetflixApp} from 'components/NetflixApp'
+import {ErrorBoundary} from 'react-error-boundary'
+import ErrorFallback from './components/ErrorFallback'
+import Error404 from 'components/Error404'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {NetflixById} from 'components/NetflixById'
+import {NetflixMovies} from 'components/NetflixMovies'
+import {NetflixSeries} from 'components/NetflixSeries'
+import {NetflixNews} from 'components/NetflixNews'
+
+function AuthApp({logout}) {
+  return (
+    <Router>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Routes>
+          <Route path="/" element={<NetflixApp logout={logout} />} />
+          <Route path="/tv/:tvId" element={<NetflixById logout={logout} />} />
+          <Route
+            path="/movie/:movieId"
+            element={<NetflixById logout={logout} />}
+          />
+          <Route path="/movies" element={<NetflixMovies logout={logout} />} />
+          <Route path="/series" element={<NetflixSeries logout={logout} />} />
+          <Route path="/news" element={<NetflixNews logout={logout} />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </ErrorBoundary>
+    </Router>
+  )
+}
+
+export {AuthApp}
