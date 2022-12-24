316ad69abcc1e6be2e7c309aeb04febb74f389fc
Thomas Fouquert <tfouquert@gmail.com>
2022-12-24 11:13:21 +0100

    update

--- src/UnauthApp.js
+++ src/UnauthApp.js
@@ -1,38 +1,31 @@
 import * as React from 'react'
-// 🐶 importe le composant 'LoginRegister' qui est dans ./components/LoginRegister
+import LoginRegister from './components/LoginRegister'
 
-// 🐶 passe les props 'login' et 'register' qui sont utilise à <Login>
-function UnauthApp() {
-  // 👨‍✈️ hugo souhaite une image de fond avec des films Netflix pour page <LoginRegister />
-  // utilise celle ci :
-  // 🤖 const imageUrl = '/images/posters.jpg'
+function UnauthApp({login, register}) {
+  const imageUrl = '/images/posters.jpg'
   return (
-    // 🐶 applique ce style pour avoir l'image de fond
-    // backgroundImage: `url('${imageUrl}')`,
-    // backgroundSize: 'cover',
+    <div
+      style={{
+        backgroundImage: `url('${imageUrl}')`,
+        backgroundSize: 'cover',
 
-    // position: 'fixed',
-    // top: 0,
-    // left: 0,
-    // bottom: 0,
-    // right: 0,
-    // overflow: 'auto',
-    <div style={{color: 'black'}}>
-      {/* 🐶 créé un logo : une balise <img> /images/netflix-logo.png
-     - margin à '30px
-     - height à 50
-    */}
-
-      <h1>lol</h1>
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        overflow: 'auto',
+      }}
+    >
+      <img
+        src="/images/netflix-logo.png"
+        alt=""
+        style={{margin: '30px'}}
+        height={50}
+      />
 
       <div>
-        {/* ⛏️ supprime le texte ci dessous et remplace le par le composant <LoginRegister>
-      Passe les props :
-      - open={true}
-      - login
-      - register
-       */}
-        Vous n'êtes pas connecté
+        <LoginRegister open={true} login={login} register={register} />
       </div>
     </div>
   )
