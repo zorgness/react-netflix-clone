384125849651ffd3e2b5609cd347642b224bad1a
Thomas Fouquert <tfouquert@gmail.com>
2022-12-25 11:00:26 +0100

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,28 @@ import CircularProgress from '@mui/material/CircularProgress'
 import 'App.css'
 import {QueryClient, QueryClientProvider} from 'react-query'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      retryDelay: 500,
+      retry: (failureCount, error) => {
+        if (error.status === 404) return false
+        else if (error.status === 401) return false
+        else if (failureCount > 3) return false
+        else return true
+      },
+    },
+    mutations: {
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      retryDelay: 500,
+      retry: 1,
+      // mutation options
+    },
+  },
+})
 
 const theme = createTheme({
   palette: {
