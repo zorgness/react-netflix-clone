6d40b1e7c321cfc5b02cfbc792659e0d159a6743
Thomas Fouquert <tfouquert@gmail.com>
2022-12-27 13:45:38 +0100

    update

--- src/components/NetflixAppBar.js
+++ src/components/NetflixAppBar.js
@@ -1,14 +1,13 @@
-import React, {useState, useEffect} from 'react'
+import * as React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import netflixLogo from '../images/netflix-logo.png'
-import netflixAvatar from '../images/netflix-avatar.png'
 import {Link} from 'react-router-dom'
 import SearchIcon from '@mui/icons-material/Search'
 import {styled, alpha} from '@mui/material/styles'
 import InputBase from '@mui/material/InputBase'
 import {useNavigate} from 'react-router-dom'
+import {useAuth} from '../context/AuthContext'
 
 const Search = styled('div')(({theme}) => ({
   marginRight: '10px',
@@ -53,16 +52,16 @@ const StyledInputBase = styled(InputBase)(({theme}) => ({
   },
 }))
 
-const NetflixAppBar = ({logout}) => {
+const NetflixAppBar = () => {
+  const {logout} = useAuth()
   const navigate = useNavigate()
-  const appBarIntialStyle = {
-    background: 'none',
-    boxShadow: 'none',
-  }
 
-  const [appBarStyle, setAppBarStyle] = useState(appBarIntialStyle)
+  const [appBarStyle, setAppBarStyle] = React.useState({
+    background: 'transparent',
+    boxShadow: 'none',
+  })
   const [query, setQuery] = React.useState('')
-  useEffect(() => {
+  React.useEffect(() => {
     const onScroll = e => {
       if (e.target.documentElement.scrollTop >= 100) {
         setAppBarStyle({
@@ -82,65 +81,65 @@ const NetflixAppBar = ({logout}) => {
 
     return () => window.removeEventListener('scroll', onScroll)
   }, [])
-  const margin10 = {margin: 10}
 
   const handleKeyPress = e => {
     if (e.keyCode === 13) {
       navigate(`/search/${query}`)
     }
   }
+
+  const margin10 = {margin: 10}
   return (
-    <>
-      <AppBar style={appBarStyle}>
-        <Toolbar>
-          <img className="nav__logo" src={netflixLogo} alt="" />
-          <Link to="/">
-            <Typography style={margin10} variant="h6">
-              Acceuil
-            </Typography>
-          </Link>
-          <Link to="/series">
-            <Typography style={margin10} variant="h6">
-              Serie
-            </Typography>
-          </Link>
-          <Link to="/movies">
-            <Typography style={margin10} variant="h6">
-              Films
-            </Typography>
-          </Link>
-          <Link to="/news">
-            <Typography style={margin10} variant="h6">
-              Nouveautés les plus regardées
-            </Typography>
-          </Link>
-          <Link to="/list">
-            <Typography style={margin10} variant="h6">
-              Ma liste
-            </Typography>
-          </Link>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              onKeyDown={handleKeyPress}
-              onChange={e => setQuery(e.target.value)}
-              value={query}
-              placeholder="Rechercher"
-              inputProps={{'aria-label': 'search'}}
-            />
-          </Search>
-          <img
-            style={{marginLeft: 'auto'}}
-            className="nav__avatar"
-            src={netflixAvatar}
-            alt=""
-            onClick={logout}
+    <AppBar style={appBarStyle}>
+      <Toolbar>
+        <img className="nav__logo" src="/images/netflix-logo.png" alt="" />
+        <Link to="/">
+          <Typography style={margin10} variant="h6">
+            Acceuil
+          </Typography>
+        </Link>
+        <Link to="/series">
+          <Typography style={margin10} variant="h6">
+            Serie
+          </Typography>
+        </Link>
+        <Link to="/movies">
+          <Typography style={margin10} variant="h6">
+            Films
+          </Typography>
+        </Link>
+        <Link to="/news">
+          <Typography style={margin10} variant="h6">
+            Nouveautés les plus regardées
+          </Typography>
+        </Link>
+        <Link to="/list">
+          <Typography style={margin10} variant="h6">
+            Ma liste
+          </Typography>
+        </Link>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            onKeyDown={handleKeyPress}
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            placeholder="Rechercher"
+            inputProps={{'aria-label': 'search'}}
           />
-        </Toolbar>
-      </AppBar>
-    </>
+        </Search>
+
+        <img
+          style={{cursor: 'pointer'}}
+          className="nav__avatar"
+          src="/images/netflix-avatar.png"
+          alt=""
+          onClick={logout}
+        />
+      </Toolbar>
+    </AppBar>
   )
 }
 
