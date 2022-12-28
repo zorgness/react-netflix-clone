080664416483b7acd22cb95d9d10b0bdd11ff4f4
Thomas Fouquert <tfouquert@gmail.com>
2022-12-28 10:18:31 +0100

    update

---
+++ src/components/LoadingFullScreen.js
@@ -0,0 +1,35 @@
+import Backdrop from '@mui/material/Backdrop'
+import CircularProgress from '@mui/material/CircularProgress'
+function LoadingFullScreen() {
+  return (
+    <div
+      style={{
+        //backgroundImage: `url('${imageUrl}')`,
+        backgroundSize: 'cover',
+
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        overflow: 'auto',
+      }}
+    >
+      <div
+        role="alert"
+        style={{
+          height: '100%',
+          textAlign: 'center',
+          padding: '100px 300px',
+          color: '#fff',
+        }}
+      >
+        <Backdrop open={true}>
+          <CircularProgress color="primary" />
+        </Backdrop>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingFullScreen
